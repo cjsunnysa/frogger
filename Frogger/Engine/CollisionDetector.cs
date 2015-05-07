e7b40cf6f35ec8380fc8c3c4332b0160831b6266
Chris Jones <cjsunnysa@hotmail.com>
2015-05-07 10:37:48 +0200

    remove debugging output from collision detector.

--- Frogger/Engine/CollisionDetector.cs
+++ Frogger/Engine/CollisionDetector.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using ChrisJones.Frogger.Interfaces;
 
 namespace ChrisJones.Frogger.Engine
@@ -8,23 +9,7 @@ namespace ChrisJones.Frogger.Engine
     {
         public bool CheckForCollisions(IHitTestable testObject, IEnumerable<IHitTestable> collidableObjects)
         {
-            foreach (var obj in collidableObjects)
-            {
-                if (testObject.GetHitTestArea().HasCollidedWith(obj.GetHitTestArea()))
-                {
-                    var t1 = testObject.GetHitTestArea();
-                    var t2 = testObject.GetHitTestArea();
-                    
-                    Console.WriteLine("player:");
-                    Console.WriteLine("{0}", t1);
-                    Console.WriteLine("object:");
-                    Console.WriteLine("{0}", t2);
-                    
-                    return true;
-                }
-            }
-
-            return false;
+            return collidableObjects.Any(obj => testObject.GetHitTestArea().HasCollidedWith(obj.GetHitTestArea()));
         }
     }
 }
